8064af33a9c6b8380f60c935b07f8eb2a1bf9cf0
Abildin Avtandil <avtandilabildin2@gmail.com>
2025-11-15 19:30:54 +0600

    2course coursework

---
+++ src/test/java/org/skypro/examiner2/Examiner2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.examiner2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examiner2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
